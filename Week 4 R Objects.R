571bdf3539261be5beb0fcea8ab3e7d3fcd83286
Joseph N. Karomo <jkaromo@kyu.ac.ke>
2023-01-24 12:32:18 +0300

    R Objects and operations _ a

--- Week 4 R Objects.R
+++ Week 4 R Objects.R
@@ -31,3 +31,42 @@ mode(MyNumbers)
 var(MyNumbers)
 sqrt(var(MyNumbers))
 sd(MyNumbers)
+sort(MyNumbers)
+sort(MyNumbers, decreasing = FALSE)
+sort(MyNumbers, decreasing = TRUE)
+newvec <- c(2,3,5,2,5,3,2,5,6,3)
+unique(newvec)
+
+#Lists
+vec1 <- c(25.3,"Kenya",cos(45),'a',54,62.3)
+vec1
+list1 <- list(25.3,"Kenya",cos(45),'a',54,62.3)
+list1
+
+list2 <- list(c(25,3.6,85.3),sin(60)
+              ,c("Kenya","Uganda"),
+              list("TZ",58,'a'),log(25))
+list2
+
+#Matrices
+a <- c(8,-4,0,1,6,8,-1,2,1,-2,0,5,8,4,5,4)
+A <- matrix(a, nrow = 4, byrow = T)
+A
+b <- c(6,-9,5,4,6,-8,7,2,5,6,1,4,8,2,-6,2)
+B <- matrix(b, nrow = 4, byrow = T)
+B
+
+#y=2B
+y = 2*B
+y
+#transpose A
+A
+t(A)
+#determnant
+det(A);det(B)
+#Inverse of A
+solve(A)
+#y2=2A-3B+t(B)-A^(-1)
+
+y2 <- 2*A-3*B+t(B)-solve(A)
+y2
