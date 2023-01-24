9c3c7c36c82eaf04ffb7bbf8a60137463c47d22e
Joseph N. Karomo <jkaromo@kyu.ac.ke>
2023-01-24 11:57:04 +0300

    R Objects and operations _ a

--- Week 4 R Objects.R
+++ Week 4 R Objects.R
@@ -17,3 +17,17 @@ print(more_fruits[c(2,4)])
 #Replace
 more_fruits[5] <- "Guava"
 print(more_fruits)
+
+#Length
+length(more_fruits)
+
+MyNumbers <- c(3,6,5,4,0.2,5,-6,9,-8)
+
+min(MyNumbers)
+max(MyNumbers)
+mean(MyNumbers)
+length(MyNumbers)
+mode(MyNumbers)
+var(MyNumbers)
+sqrt(var(MyNumbers))
+sd(MyNumbers)
