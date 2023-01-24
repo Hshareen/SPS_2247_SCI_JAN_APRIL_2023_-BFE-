650f66841d6de2a03793ccb1adca7ac00b7d5968
Joseph N. Karomo <jkaromo@kyu.ac.ke>
2023-01-24 11:16:49 +0300

    Week 4 class

--- Week1 (Motivation to R).R
+++ Week1 (Motivation to R).R
@@ -1,2 +1,18 @@
 # Motivation
 # why would we need R
+myvar1 <- 5+9
+myvar2 <- 6/8
+
+myvar3 <- myvar1+myvar2
+myvar4 <- myvar +myvar2
+
+A <- matrix(c(2,3,6,5),nrow = 2)
+A
+
+mydata <- read.csv("E:\\Workshops and Conferences\\KaRU\\framingham-heart-study-dataset\\framingham.csv")
+View(mydata)
+
+#Getting/Setting a working directory
+getwd()
+setwd("E:\\TF WORK\\CLASS WORKLOAD\\SEM 2\\JAN-APRIL 2023\\SPS_2247_SCI_JAN_APRIL_2023_-BFE-")
+setwd("E:/TF WORK/CLASS WORKLOAD/SEM 2/JAN-APRIL 2023/SPS_2247_SCI_JAN_APRIL_2023_-BFE-")
